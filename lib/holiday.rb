b5780e18724decb2f37ef2374e41be760bc38761
Nathan Steed <steedn@outlook.com>
2020-08-03 08:33:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,8 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash.each do |a, d|
-  a.values.flatten
+  if a == :winter
+    a.values.flatten
     end
 end
 
