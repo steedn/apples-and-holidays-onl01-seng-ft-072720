b4f8baeea83a1f96a3e15cd70ad56467a22ca8a9
Nathan Steed <steedn@outlook.com>
2020-08-03 08:12:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,6 @@ def add_supply_to_memorial_day(holiday_hash, supply)
      end
     end
    end
-end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
